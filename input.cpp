c8b77c793c1d66693453cfae72fd318dd5030e5f
Joy Chandra Uday <joychandraud@gmail.com>
2025-08-12 02:26:03 +0600

    Refactor input handling to allow simultaneous entry of two numbers and add initial VSCode settings

--- input.cpp
+++ input.cpp
@@ -5,10 +5,15 @@ int main()
 {
     int number1, number2;
 
-    cout << "Please enter number 1: ";
-    cin >> number1;
-    cout << "Please enter number 2: ";
-    cin >> number2;
+    // taking multiple valueseperately
+    //  cout << "Please enter number 1: ";
+    //  cin >> number1;
+    //  cout << "Please enter number 2: ";
+    //  cin >> number2;
+
+    // taking multiple value at a time
+    cout << "Please enter 2 number: ";
+    cin >> number1 >> number2;
 
     cout << "The sum of the number is: " << number1 + number2 << endl;
 
