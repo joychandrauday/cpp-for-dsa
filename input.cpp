59b7405b0eafeb762bfd6e0d3fee2611c13e1a9f
Joy Chandra Uday <joychandraud@gmail.com>
2025-08-12 02:23:44 +0600

    Update input program to accept two numbers and display their sum

--- input.cpp
+++ input.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main()
 {
-    int number;
+    int number1, number2;
 
-    cout << "Please enter a number: ";
-    cin >> number;
+    cout << "Please enter number 1: ";
+    cin >> number1;
+    cout << "Please enter number 2: ";
+    cin >> number2;
 
-    cout << "The number you entered is: " << number << endl;
+    cout << "The sum of the number is: " << number1 + number2 << endl;
 
     return 0;
 }
