36ba80e258c4950f0d31eaf19abf92dd43047744
Joy Chandra Uday <joychandraud@gmail.com>
2025-08-12 03:11:00 +0600

    Refactored

--- arithmatic-operator.cpp
+++ arithmatic-operator.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <iomanip> // for setw, fixed, setprecision
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -7,31 +7,56 @@ int main()
     int dif, sum, num1, num2, modulus;
     double multi, division;
 
-    cout << setw(15) << "Enter two numbers: ";
+    cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
     sum = num1 + num2;
-    cout << setw(15) << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
-
     dif = num1 - num2;
-    cout << setw(15) << "The difference of " << num1 << " and " << num2 << " is " << dif << endl;
-
     multi = num1 * num2;
-    cout << setw(15) << "The multiplication of " << num1 << " and " << num2 << " is " << multi << endl;
+
+    cout << fixed << setprecision(2); // set decimal precision globally
+
+    // Print header (optional)
+    cout << left << setw(20) << "Operation"
+         << right << setw(15) << "Num1"
+         << setw(15) << "Num2"
+         << setw(20) << "Result" << endl;
+
+    cout << string(70, '-') << endl; // separator line
+
+    cout << left << setw(20) << "Sum"
+         << right << setw(15) << num1
+         << setw(15) << num2
+         << setw(20) << sum << endl;
+
+    cout << left << setw(20) << "Difference"
+         << right << setw(15) << num1
+         << setw(15) << num2
+         << setw(20) << dif << endl;
+
+    cout << left << setw(20) << "Multiplication"
+         << right << setw(15) << num1
+         << setw(15) << num2
+         << setw(20) << multi << endl;
 
     if (num2 != 0)
     {
         division = static_cast<double>(num1) / num2;
-
-        cout << setw(15) << fixed << setprecision(2); // set decimal precision to 2
-        cout << setw(15) << "The division of " << num1 << " by " << num2 << " is " << division << endl;
-
         modulus = num1 % num2;
-        cout << setw(15) << "The modulus of " << num1 << " and " << num2 << " is " << modulus << endl;
+
+        cout << left << setw(20) << "Division"
+             << right << setw(15) << num1
+             << setw(15) << num2
+             << setw(20) << division << endl;
+
+        cout << left << setw(20) << "Modulus"
+             << right << setw(15) << num1
+             << setw(15) << num2
+             << setw(20) << modulus << endl;
     }
     else
     {
-        cout << setw(15) << "Division and modulus by zero are not allowed." << endl;
+        cout << "Division and modulus by zero are not allowed." << endl;
     }
 
     return 0;
